bbd348f4ff05e6cad9b2cc3fc05b2929eeb36928
makuwa <xmakuwwa@gmail.com>
2024-06-13 18:34:59 +0200

    private property added

--- 6.6.2.js
+++ 6.6.2.js
@@ -1,17 +1,19 @@
 class Storage {
+	#items
 	constructor(items) {
-		this.items = items;
+		this.#items = items;
 	}
 	getItems() {
-		return this.items;
+		return this.#items;
 	}
+	
 	addItem(newItem) {
-		return this.items.push(newItem);
+		return this.#items.push(newItem);
 	}
 	removeItem(itemToRemove) {
-		const index = this.items.indexOf(itemToRemove);
+		const index = this.#items.indexOf(itemToRemove);
 		if (index !== -1) {
-			this.items.splice(index, 1);
+			this.#items.splice(index, 1);
 		}
 	}
 }
